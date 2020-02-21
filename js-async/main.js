819dcea106b765c9fb426ebbd64de2d29f0a630d
dagorik <dagorik@gmail.com>
2020-02-20 21:34:45 -0600

    add js-async

---
+++ js-async/main.js
@@ -0,0 +1,28 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+
+// console.log(1);
+// setTimeout(function(){
+//     console.log(2);
+// },10000);
+// setTimeout(function(){
+//     console.log(3);
+// },5000);
+// console.log(4);
+
+// 1 , 4, 3, 2
+
+console.log(1);
+setTimeout(function(){
+    console.log(2);
+},100);
+for(let i = 0; i<9999999999; i++);
+console.log(3);
+// 1, 3, 2
+
+
+// callback
+// -> Función anonima que se pasa como parametro
+
