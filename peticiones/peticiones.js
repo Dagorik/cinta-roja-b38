e5be492f49be2ec1f91209163b9a8f47bec41c28
dagorik <dagorik@gmail.com>
2020-02-24 21:50:14 -0600

    meteoritos

--- peticiones/peticiones.js
+++ peticiones/peticiones.js
@@ -10,5 +10,19 @@ function getPeopleById(peopleId){
 }
 
 
-
+console.log(1);
 getPeopleById(14);
+console.log(2);
+
+
+// PARA BUSCAR LOS METEORITOS
+// json.near_earth_objects --> obj
+// json["near_earth_objects"] --> obj
+
+// json.2020-12-02
+// json["2020-12-02"]
+
+// json.primer nombre
+// json["primer nombre"]
+
+// Object.keys()
