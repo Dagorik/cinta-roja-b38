0aba6099181635cf0b4be643d8757777a8261b28
dagorik <dagorik@gmail.com>
2020-03-02 20:05:35 -0600

    sswapi

--- promesas/swapi.js
+++ promesas/swapi.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+// // Imprimir el nombre de el personaje y su primera pelicula
 // function getPeopleByIdVersionFea(idPeople){
 //     request.get(`https://swapi.co/api/people/${idPeople}/`, (err, response, body) => {
 //         console.log(err);
@@ -20,17 +21,28 @@ const request = require('request');
 // getPeopleByIdVersionFea(20);
 
 
+// --> Se ejcuta ---> Main --> PRESENTE
+//     --> Se va a la cola
+//         --> Pila
+//         <-- Pila - Callback ---> FUTURO
+//     --> callback se ejecuta - Se ejecuta la segunda peticion
+//         -->  Pila
+//         <-- Pila - Callback ---> MAS FUTURO
+//     Se imprime
 
 
 
+
+// Definición de la función
 function getPeopleById(idPeople) {
+    // Definición de la promesa
     return new Promise((resolve, reject) => {
         request.get(`https://swapi.co/api/people/${idPeople}/`, (err, response, body) => {
             if (response.statusCode === 200){
                 const json = JSON.parse(body);
-                resolve(json)
+                resolve(json);
             } else {
-                reject('Error en tu petición de people');
+                reject('Petición incorrecta');
             }
         })
     });
@@ -50,9 +62,9 @@ function getMovieByUrl(urlMovie){
     });
 }
 
-getPeopleById(20)
+getPeopleById(2)
     .then((people) => {
-        console.log(people.name);
+        console.log('THEN', people.name);
         return getMovieByUrl(people.films[0])
     })
     .then((movie) => {
@@ -63,8 +75,51 @@ getPeopleById(20)
     })
 
 
-    // Version resumida
-getPeopleById(20)
-    .then((people) => getMovieByUrl(people.films[0]))
-    .then((movie) => console.log(movie.title))
-    .catch((err) => console.log('CATCH', err))
+
+
+
+// function getPeopleById(idPeople) {
+//     return new Promise((resolve, reject) => {
+//         request.get(`https://swapi.co/api/people/${idPeople}/`, (err, response, body) => {
+//             if (response.statusCode === 200){
+//                 const json = JSON.parse(body);
+//                 resolve(json)
+//             } else {
+//                 reject('Error en tu petición de people');
+//             }
+//         })
+//     });
+// }
+
+
+// function getMovieByUrl(urlMovie){
+//     return new Promise((resolve, reject) => {
+//         request.get(urlMovie, (err, response, body) => {
+//             if (response.statusCode === 200){
+//                 const json = JSON.parse(body);
+//                 resolve(json)
+//             } else {
+//                 reject('Error en tu petición de movies');
+//             }
+//         })
+//     });
+// }
+
+// getPeopleById(20)
+//     .then((people) => {
+//         console.log(people.name);
+//         return getMovieByUrl(people.films[0])
+//     })
+//     .then((movie) => {
+//         console.log(movie.title);
+//     })
+//     .catch((err) => {
+//         console.log('CATCH', err);
+//     })
+
+
+//     // Version resumida
+// getPeopleById(20)
+//     .then((people) => getMovieByUrl(people.films[0]))
+//     .then((movie) => console.log(movie.title))
+//     .catch((err) => console.log('CATCH', err))
